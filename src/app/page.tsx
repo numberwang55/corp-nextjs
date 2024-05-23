d8ee0327ae153f50ffb4f486238a2fcd522a68d9
numberwang55 <cban0901@hotmail.com>
2024-05-23 10:58:21 +0100

    Created Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-import Link from "next/link";
-
 export default function Home() {
-  return (
-    <div>
-      <div className="grid">
-        <Link href="/performance">Performance</Link>
-        <Link href="/reliability">reliability</Link>
-        <Link href="/scale">scale</Link>
-      </div>
-      Home Page
-    </div>
-  );
+  return <div>Home Page</div>;
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Header() {
+  return <div className="grid">
+    <Link href="/performance">Performance</Link>
+    <Link href="/reliability">reliability</Link>
+    <Link href="/scale">scale</Link>
+  </div>;
+}
