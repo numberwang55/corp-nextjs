a2dcd7b6f091201704810c331c2618513a0e6867
numberwang55 <cban0901@hotmail.com>
2024-05-29 19:51:42 +0100

    added home link to header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return <div className="grid">
+    <Link href="/">Home</Link>
     <Link href="/performance">Performance</Link>
     <Link href="/reliability">reliability</Link>
     <Link href="/scale">scale</Link>
