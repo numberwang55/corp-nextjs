668194075887225b165f0015a7dafdc123265817
numberwang55 <cban0901@hotmail.com>
2024-06-16 10:43:54 +0100

    header styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
 
 export default function Header() {
-  return <div className="grid">
-    <Link href="/">Home</Link>
-    <Link href="/performance">Performance</Link>
-    <Link href="/reliability">reliability</Link>
-    <Link href="/scale">scale</Link>
-  </div>;
+  return (
+    <div className="w-full absolute text-white z-10">
+      <nav className="container relative flex flex-wrap items-center justify-between mx-auto p-8">
+        <Link href="/" className="font-bold text-3xl">
+          Home
+        </Link>
+        <div className="space-x-4 text-xl">
+          <Link href="/performance">Performance</Link>
+          <Link href="/reliability">reliability</Link>
+          <Link href="/scale">scale</Link>
+        </div>
+      </nav>
+    </div>
+  );
 }
-
